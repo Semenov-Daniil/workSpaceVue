32e7cc4555ed7824ed57f6fd42648dd0982b9db7
Semenov-Daniil <ds.daniilsemen.ds@gmail.com>
2023-11-18 15:25:15 +0300

    update pop up

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -22,7 +22,7 @@
               </svg>
             </div>
           </div>
-          <input form="filter__form" type="reset" value="Очистить" class="filter__form__reset form-title__reset" />
+          <input form="filter__form" type="reset" value="Очистить" class="filter__form__reset form-title__reset" @click="reset"/>
           <!-- <button class="filter__form__reset" @click="reset">Очистить</button> -->
       </div>
 
@@ -102,43 +102,50 @@
   
           <div class="filter__form__operation">
             <input type="submit" class="filter__form__submit btn" value="Применить" @click="submit"/>
-            <input type="reset" class="filter__form__reset form__operation__reset" value="Очистить" />
+            <input type="reset" class="filter__form__reset form__operation__reset" value="Очистить" @click="reset"/>
           </div> 
         </form>
       </transition>
     </aside>
     <work-list
       :workList="workList"
-      @popup="console.log($event)"
+      @popup="openPopup"
     />
-    <div class="wrapper__popup">
-      <div class="popup">
+    <div class="wrapper__popup" v-show="companyShow" @click="closePopup">
+      <div class="popup" @click.stop>
         <div class="popup__title">
-          <img src="img/work_company/logo_company_3.png" alt="img" class="popup__title__img" />
+          <img 
+            :src="'https://workspace-methed.vercel.app/' + company.logo" 
+            :alt="company.logo" 
+            class="popup__title__img"
+          />
           <div class="popup__title__info">
-            <p>Creative People</p>
-            <span>Графический дизайнер</span>
+            <p class="popup__title__company">{{ company.company }}</p>
+            <span class="popup__title__vacancy">{{ company.title }}</span>
           </div>
         </div>
         <div class="popup__info">
-          Привет. Мы в CreativePeople ищем middle графического дизайнера в свою
-          дизайн команду. Удаленно, из любой точки нашей страны, где у вас будет
-          хороший интернет. Опыт работы в разработке логотипов, фирменных стилей
-          обязателен.<br />
-          У нас в портфолио много крупных российских компаний, с некоторыми
-          мы работаем уже много лет и делаем самые разные проекты, от сайтов
-          до мобильных приложений.
-        </div>
-        <div class="popup__tags">
-          <span class="popup__tags__item"> от 110 000₽ </span>
-          <span class="popup__tags__item"> проектная работа </span>
-          <span class="popup__tags__item"> удаленный </span>
-          <span class="popup__tags__item"> опыт от 1 года до 3-х лет </span>
-          <span class="popup__tags__item"> Москва </span>
+          <p class="popup__info__desc">
+            {{(company.description) }}
+          </p>
+          <ul class="popup__tags">
+            <li class="popup__tags__item">от {{ (Number(company.salary)).toLocaleString("ru-RU") }}₽</li>
+            <li class="popup__tags__item">{{ company.type }}</li>
+            <li class="popup__tags__item">{{ company.format }}</li>
+            <li class="popup__tags__item">опыт {{ company.experience }}</li>
+            <li class="popup__tags__item">{{ company.location }}</li>
+          </ul>
         </div>
         <div class="popup__link">
-          Отправляйте резюме на
-          <a href="mailto:CreativePeople@gmail.com">CreativePeople@gmail.com</a>
+          Отправляйте резюме на <a :href="'mailto:' + company.email">{{ company.email }}</a>
+        </div>
+        <div class="likemen">
+          <img src="img/site/likemen.png" alt="likemen">
+        </div>
+        <div class="close_icon" @click="closePopup">
+          <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6.78335 6L11.3889 1.39444C11.4799 1.28816 11.5275 1.15145 11.5221 1.01163C11.5167 0.871815 11.4587 0.739182 11.3598 0.640241C11.2608 0.541299 11.1282 0.483337 10.9884 0.477936C10.8486 0.472535 10.7118 0.520094 10.6056 0.611109L6.00001 5.21666L1.39446 0.605553C1.28984 0.50094 1.14796 0.442169 1.00001 0.442169C0.852067 0.442169 0.710181 0.50094 0.605568 0.605553C0.500955 0.710167 0.442183 0.852053 0.442183 0.999998C0.442183 1.14794 0.500955 1.28983 0.605568 1.39444L5.21668 6L0.605568 10.6056C0.547411 10.6554 0.500178 10.7166 0.466831 10.7856C0.433485 10.8545 0.414746 10.9296 0.411791 11.0061C0.408836 11.0826 0.421728 11.1589 0.449658 11.2302C0.477588 11.3015 0.519954 11.3662 0.574095 11.4204C0.628237 11.4745 0.692985 11.5169 0.764277 11.5448C0.835569 11.5727 0.911865 11.5856 0.988375 11.5827C1.06489 11.5797 1.13996 11.561 1.20888 11.5276C1.27781 11.4943 1.3391 11.447 1.3889 11.3889L6.00001 6.78333L10.6056 11.3889C10.7118 11.4799 10.8486 11.5275 10.9884 11.5221C11.1282 11.5167 11.2608 11.4587 11.3598 11.3598C11.4587 11.2608 11.5167 11.1282 11.5221 10.9884C11.5275 10.8485 11.4799 10.7118 11.3889 10.6056L6.78335 6Z"/>
+          </svg>
         </div>
       </div>
     </div>
@@ -198,6 +205,9 @@ export default {
       homeUrl: 'https://workspace-methed.vercel.app/',
 
       filterShow: true,
+      companyShow: false,
+
+      company: {},
     }
   },
   methods: {
@@ -278,6 +288,21 @@ export default {
       } catch(err) {
         alert(err);
       }
+    },
+
+    async openPopup(event) {
+      try {
+        let response = await fetch('https://workspace-methed.vercel.app/api/vacancy/' + event);
+        this.company = (await response.json());
+        this.companyShow = true;
+      } catch(err) {
+        alert(err);
+      }
+    },
+
+    closePopup() {
+      this.company = {};
+      this.companyShow = false;
     }
   },
   computed: {
